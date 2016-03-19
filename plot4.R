a137e84f36dcc0f709f5907140d50a7eb523fb64
d-roy <debsubhra.roy@gmail.com>
2016-03-18 23:01:03 -0700

    Added files via upload

---
+++ plot4.R
@@ -0,0 +1,31 @@
+library(plyr)
+library(ggplot2)
+
+#NEI <- readRDS("summarySCC_PM25.rds")
+#SCC <- readRDS("Source_Classification_Code.rds")
+
+CoalCombustionSCC <- subset(SCC, EI.Sector %in% c("Fuel Comb - 
+                            Comm/Institutional - Coal",
+                                                  "Fuel Comb - Electric Generation - Coal",
+                                                  "Fuel Comb - Industrial Boilers, ICEs - Coal"))
+# Compare to Short.Name matching both Comb and Coal
+CoalCombustionSCC1 <- subset(SCC, grepl("Comb", Short.Name) & grepl("Coal", 
+                                                                    Short.Name))
+
+
+
+CoalCombustionSCCCodes <- union(CoalCombustionSCC$SCC, CoalCombustionSCC1$SCC)
+length(CoalCombustionSCCCodes)
+
+
+CoalCombustion <- subset(NEI, SCC %in% CoalCombustionSCCCodes)
+
+coalCombustionPM25ByYear <- ddply(CoalCombustion, .(year, type), function(x) 
+  sum(x$Emissions))
+colnames(coalCombustionPM25ByYear)[3] <- "Emissions"
+
+qplot(year, Emissions, data = coalCombustionPM25ByYear, color = type, 
+      geom = "line") + ggtitle(expression("Coal Combustion" ~ PM[2.5] ~ 
+                                            "Emissions by Source Type and Year")) + xlab("Year") + 
+  ylab(expression  ("Total" ~ PM[2.5] ~ "Emissions (tons)"))
+
